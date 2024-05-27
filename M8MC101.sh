680afa9427f7772bc94cc06b96a9a725856dd07e
Takayuki Iwasaki <sakisan@st.rim.or.jp>
2024-05-27 18:26:13 +0900

    Add files via upload

---
+++ M8MC101.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Uncomment the following lines to disable wi-fi
+#sudo modprobe -r mt7601u
+#sudo sed -i '$ablacklist mt7601u' /etc/modprobe.d/blacklist.conf
+#printf "\n\n\n\e[32mWifi has been disabled.\n"
+
+sed -i "/^idle_ms=/s/=.*/=25/" ~/.local/share/m8c/config.ini
+
+m8_orgpath=ports/M8
+m8_path=1M8ARK
+roms2_path=/roms2
+roms_path=/roms
+path1=$roms_path/$m8_orgpath
+path2=$roms2_path/$m8_orgpath
+path3=$roms_path/$m8_path
+path4=$roms2_path/$m8_path
+
+if [ -e "$path1" ]; then
+  cd $path1
+elif [ -e "$path2" ]; then
+  cd $path2
+elif [ -e "$path3" ]; then
+  cd $path3
+elif [ -e "$path4" ]; then
+  cd $path4
+fi
+
+# set cpu governor to powersave to minimize audio "crackles"
+#echo "powersave" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+
+# alsaloop_wait will run in background and try to create the loopback 
+# if it can't be created before m8c runs. Useful for wait_for_device=true. 
+#./alsaloop_wait &
+#alsaloop -P hw:2,0 -C hw:1,0 -t 200000 -A 5 --rate 44100 --sync=0 -T -1 -d
+alsaloop -P plughw:MC101,0 -C hw:M8,0 -t 200000 --rate 44100 -A 5 -T -1 -d
+sleep 1
+./_m8c/m8c
+
+pkill alsaloop
+
+# Uncomment the following lines to enable wi-fi
+#sudo modprobe -i mt7601u
+#sudo sed -i '/blacklist mt7601u/d' /etc/modprobe.d/blacklist.conf
+#printf "\n\n\n\e[32mWifi has been enabled.\n"
