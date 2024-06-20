b4240a7740a97cdf18b200fad85003d37afe930b
Takayuki Iwasaki <sakisan@st.rim.or.jp>
2024-06-20 19:14:57 +0900

    Update start_fluidsynth.sh

--- experimental/start_fluidsynth.sh
+++ experimental/start_fluidsynth.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 
 fluidsynth -s -i -a alsa -o audio.alsa.device=hw:M8,0 -m alsa_seq /usr/share/sounds/sf3/default-GM.sf3 &
+#GeneralUser GS
+#fluidsynth -s -i -a alsa -o audio.alsa.device=hw:M8,0 -m alsa_seq /roms2/1M8ARK/'GeneralUser GS v1.471.sf2' &
+
 sleep 2
 
 m8=$(aconnect -i -l | awk '/M8/{ print substr($2,1,length($2)-1);exit }')
