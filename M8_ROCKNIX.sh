a79bb7fac956ddb408985d76bad392bac0b5e7be
Takayuki Iwasaki <sakisan@st.rim.or.jp>
2024-08-03 01:39:56 +0900

    Update M8_ROCKNIX.sh bug fix

--- M8_ROCKNIX.sh
+++ M8_ROCKNIX.sh
@@ -4,10 +4,11 @@ cd /storage/roms/ports/M8
 
 #./alsaloop -P hw:rk817ext,0 -C hw:M8,0 -t 200000 -A 5 --rate 44100 --sync=0 -T -1 -d
 
-M8=$(pa-info 2>&1 | awk '/alsa_input\.usb-DirtyWave_M8_[0-9\-].*\.analog-stereo/{print $3;exit}')
+M8=$(pa-info 2>&1 | awk '/Name: alsa_input\.usb-DirtyWave_M8_[0-9\-].*\.analog-stereo/{print $2;exit}')
+SPK=$(pa-info 2>&1 | awk '/Name: alsa_output\._sys_devices_platform_.*HiFi__Headphones__sink/{print $2;exit}')
 pactl set-default-source ${M8}
-pactl set-default-sink alsa_output._sys_devices_platform_sound_sound_card1.HiFi__Headphones__sink
-pactl load-module module-loopback source=${M8} alsa_output._sys_devices_platform_sound_sound_card1.HiFi__Headphones__sink
+pactl set-default-sink ${SPK}
+pactl load-module module-loopback source=${M8} sink=${SPK}
 sleep 2
 ./_m8c/m8c
 
