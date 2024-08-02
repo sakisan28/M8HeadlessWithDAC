0869916fa6c9551eb5821d894077878bd45477c9
Takayuki Iwasaki <sakisan@st.rim.or.jp>
2024-08-03 01:52:59 +0900

    Update M8_ROCKNIX.sh

--- M8_ROCKNIX.sh
+++ M8_ROCKNIX.sh
@@ -8,7 +8,7 @@ M8=$(pa-info 2>&1 | awk '/Name: alsa_input\.usb-DirtyWave_M8_[0-9\-].*\.analog-s
 SPK=$(pa-info 2>&1 | awk '/Name: alsa_output\._sys_devices_platform_.*HiFi__Headphones__sink/{print $2;exit}')
 pactl set-default-source ${M8}
 pactl set-default-sink ${SPK}
-pactl load-module module-loopback source=${M8} sink=${SPK}
+pactl load-module module-loopback source=${M8} sink=${SPK} latency_msec=200 format=s16le rate=44100 channels=2
 sleep 2
 ./_m8c/m8c
 
