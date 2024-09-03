ca5f73641337678bb441a1858ff5a87f41aca1af
Takayuki Iwasaki <sakisan@st.rim.or.jp>
2024-09-03 03:22:38 -0400

    script update to simple

--- specific/M8_USBINOUT_SoundBlasterPlay4.sh
+++ specific/M8_USBINOUT_SoundBlasterPlay4.sh
@@ -10,24 +10,7 @@
 
 sed -i "/^idle_ms=/s/=.*/=25/" ~/.local/share/m8c/config.ini
 
-m8_orgpath=ports/M8
-m8_path=1M8ARK
-roms2_path=/roms2
-roms_path=/roms
-path1=$roms_path/$m8_orgpath
-path2=$roms2_path/$m8_orgpath
-path3=$roms_path/$m8_path
-path4=$roms2_path/$m8_path
-
-if [ -e "$path1" ]; then
-  cd $path1
-elif [ -e "$path2" ]; then
-  cd $path2
-elif [ -e "$path3" ]; then
-  cd $path3
-elif [ -e "$path4" ]; then
-  cd $path4
-fi
+cd $(dirname $0)
 
 # set cpu governor to powersave to minimize audio "crackles"
 #echo "powersave" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
