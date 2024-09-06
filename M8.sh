5b2c3e93caf843338dbc0c170a088cfa0f4f7e9e
Takayuki Iwasaki <sakisan@st.rim.or.jp>
2024-09-06 07:08:30 -0400

    check ROCKNIX

--- M8.sh
+++ M8.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-# Uncomment the following lines to disable wi-fi
-#sudo modprobe -r mt7601u
-#sudo sed -i '$ablacklist mt7601u' /etc/modprobe.d/blacklist.conf
-#printf "\n\n\n\e[32mWifi has been disabled.\n"
-
 sed -i "/^idle_ms=/s/=.*/=25/" ~/.local/share/m8c/config.ini
 
 cd $(dirname $0)
-
-# set cpu governor to powersave to minimize audio "crackles"
-#echo "powersave" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-#echo "performance" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 
 aplay -l | grep 'rockchiprk817co'
 
@@ -24,8 +15,8 @@ then
     pkill alsaloop
 else
 #ROCKNIX
-    M8=$(pa-info 2>&1 | awk '/Name: alsa_input\.usb-DirtyWave_M8_[0-9\-].*\.analog-stereo/{print $2;exit}')
-    SPK=$(pa-info 2>&1 | awk '/Name: alsa_output\._sys_devices_platform_.*HiFi__Headphones__sink/{print $2;exit}')
+    M8=$(pactl list sources short 2>&1 | awk '/alsa_input\.usb-DirtyWave_M8/{print $1;exit}')
+    SPK=$(pactl list sinks short 2>&1 | awk '/alsa_output\._sys_devices_platform_.*HiFi__Headphones__sink/{print $1;exit}')
     pactl set-default-source ${M8}
     pactl set-default-sink ${SPK}
     pactl load-module module-loopback source=${M8} sink=${SPK} latency_msec=200 format=s16le rate=44100 channels=2
@@ -33,8 +24,3 @@ else
     ./_m8c/m8c
     pactl unload-module module-loopback
 fi
-
-# Uncomment the following lines to enable wi-fi
-#sudo modprobe -i mt7601u
-#sudo sed -i '/blacklist mt7601u/d' /etc/modprobe.d/blacklist.conf
-#printf "\n\n\n\e[32mWifi has been enabled.\n"
