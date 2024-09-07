6e9f83c33df8ae5d300394736f750f4b78a7670c
Takayuki Iwasaki <sakisan@st.rim.or.jp>
2024-09-07 11:23:28 -0400

    bug fix

--- M8_USBINOUT.sh
+++ M8_USBINOUT.sh
@@ -19,14 +19,15 @@ then
     pkill alsaloop
 else
 #ROCKNIX
-    M8=$(pactl list sources short 2>&1 | awk '/alsa_input\.usb-DirtyWave_M8/{print $1;exit}')
+    M8IN=$(pactl list sinks short 2>&1 | awk '/alsa_output\.usb-DirtyWave_M8/{print $1;exit}')
+    M8OUT=$(pactl list sources short 2>&1 | awk '/alsa_input\.usb-DirtyWave_M8/{print $1;exit}')
     USBOUT=$(pactl list sinks short 2>&1 |grep 'alsa_output\.usb'|grep -v 'DirtyWave_M8'|awk '{print $1;exit}')
     USBIN=$(pactl list sources short|grep 'alsa_input\.usb'|grep -v 'DirtyWave_M8'|awk '{print $1;exit}')
     pactl set-default-source ${M8}
     pactl set-default-sink ${USBOUT}
-    pactl load-module module-loopback source=${M8} sink=${USBOUT} latency_msec=200 format=s16le rate=44100 channels=2
+    pactl load-module module-loopback source=${M8OUT} sink=${USBOUT} latency_msec=200 format=s16le rate=44100 channels=2
     sleep 2
-    pactl load-module module-loopback source=${USBIN} sink=${M8} latency_msec=200 format=s16le rate=44100 channels=2
+    pactl load-module module-loopback source=${USBIN} sink=${M8IN} latency_msec=200 format=s16le rate=44100 channels=2
     sleep 2
     ./_m8c/m8c
     pactl unload-module module-loopback
